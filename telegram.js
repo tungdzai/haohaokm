db2d9fa2e98489f1a9d648a500a107fb26d0a392
tungdzai <87531719+tungdzai@users.noreply.github.com>
2024-11-03 22:41:25 +0700

    Update telegram.js

--- telegram.js
+++ telegram.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TELEGRAM_BOT_TOKEN;
-const tokenNTC = process.env.TELEGRAM_BOT_TOKEN_NAP_THE_CAO;
+// const tokenNTC = process.env.TELEGRAM_BOT_TOKEN_NAP_THE_CAO;
 const chatId = process.env.ID_TELEGRAM;
 const bot = new TelegramBot(token, {polling: true});
-const botNTC = new TelegramBot(tokenNTC, {polling: true});
+// const botNTC = new TelegramBot(tokenNTC, {polling: true});
 
 async function sendTelegramMessage(message) {
     try {
@@ -13,17 +13,17 @@ async function sendTelegramMessage(message) {
         console.error(`Lỗi gửi tin nhắn đến Telegram (chatId: ${chatId}):`, error);
     }
 }
-async function sendTelegramMessageNTC(message) {
-    try {
-        await botNTC.sendMessage(chatId, message);
-    } catch (error) {
-        console.error(`Lỗi gửi tin nhắn đến Telegram (chatId: ${chatId}):`, error);
-    }
-}
+// async function sendTelegramMessageNTC(message) {
+//     try {
+//         await botNTC.sendMessage(chatId, message);
+//     } catch (error) {
+//         console.error(`Lỗi gửi tin nhắn đến Telegram (chatId: ${chatId}):`, error);
+//     }
+// }
 
 module.exports = {
     sendTelegramMessage,
-    sendTelegramMessageNTC,
     bot,
-    botNTC
+    // sendTelegramMessageNTC,
+    // botNTC
 };
